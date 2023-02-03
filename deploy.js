53b6ea837d37d56d85a9d341925a3e4a985e5bac
Muhammad Zubair <m.zobaire@gmail.com>
2023-02-03 13:39:29 +0500

    retrieving number from contract

--- deploy.js
+++ deploy.js
@@ -10,6 +10,16 @@ async function main() {
   const bin = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
   const contractFactory = new ethers.ContractFactory(abi, bin, wallet);
   console.log("Deploying, Please wait...");
+  const contract = await contractFactory.deploy(); // Stop here! Wait for contract to deploy
+  await contract.deployTransaction.wait(1);
+  console.log(`Contract Address ${contract.address}`);
+  // Get Number
+  const currentFavoriteNumber = await contract.retrieve();
+  console.log(`Current Favorite Number: ${currentFavoriteNumber.toString()}`);
+  const transactionResponse = await contract.store("8");
+  const transactionReciept = await transactionResponse.wait(1);
+  const updateFavoriteNumber = await contract.retrieve();
+  console.log(`Update Favorite Number is: ${updateFavoriteNumber}`);
 }
 
 main()
